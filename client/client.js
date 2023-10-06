9bd6098a1d2ea239e409e3336f296eb3e60e9d22
Himanshu Kumar <i.himu.kumar23@gmail.com>
2023-10-07 04:07:17 +0530

    Dynamic content in client's static page

--- client/client.js
+++ client/client.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('dist'));
+
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/dist/index.html');
 });
