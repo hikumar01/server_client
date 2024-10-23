3596c33dbbe377b9036568337738669ed6c4889e
Himanshu Kumar <hikumar@adobe.com>
2024-10-23 20:25:58 +0530

    changed react-scripts to vite
    Both project are working fine individually. Express server needs to be coupled with vite react client.

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 // const cors = require('cors');
 
 const PORT = process.env.PORT || 8080;
-const clientFolder = '../client/build';
+const clientFolder = '../client/dist';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const clientPath = path.join(__dirname, clientFolder);
